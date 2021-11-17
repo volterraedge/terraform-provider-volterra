b21a93d8269299ac5b5dc6fd6af8e50536c9e88d
alexcohen <alexcohen@ves.io>
2021-11-17 12:46:10 -0500

    add data_source_volterra_service_policy

---
+++ volterra/data_source_volterra_service_policy.go
@@ -0,0 +1,54 @@
+//
+// Copyright (c) 2020 Volterra, Inc. Licensed under APACHE LICENSE, VERSION 2.0
+//
+
+package volterra
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+
+	ves_io_schema_service_policy "github.com/volterraedge/terraform-provider-volterra/pbgo/extschema/schema/service_policy"
+)
+
+// dataSourceVolterraServicePolicy is implementation of Volterra's Service Policy resource read
+func dataSourceVolterraServicePolicy() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceVolterraServicePolicyRead,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"tenant_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+// read volterra service_policy
+func dataSourceVolterraServicePolicyRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*APIClient)
+	name := d.Get("name").(string)
+
+	resp, err := client.GetObject(context.Background(), ves_io_schema_service_policy.ObjectType, "", name)
+	if err != nil {
+		if strings.Contains(err.Error(), "status code 404") {
+			log.Printf("[INFO] Service Policy %s no longer exists", d.Id())
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("Error finding Volterra Service Policy %q: %s", d.Id(), err)
+	}
+	d.Set("tenant_name", resp.GetObjSystemMetadata().GetTenant())
+	d.Set("name", resp.GetObjName)
+	d.SetId(resp.GetObjUid())
+	return nil
+}
---
+++ volterra/data_source_volterra_service_policy_test.go
@@ -0,0 +1,52 @@
+//
+// Copyright (c) 2020 Volterra, Inc. Licensed under APACHE LICENSE, VERSION 2.0
+//
+package volterra
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	ves_io_schema_ns "github.com/volterraedge/terraform-provider-volterra/pbgo/extschema/schema/service_policy"
+)
+
+func TestAccDataSourceServicePolicyBasic(t *testing.T) {
+	name := generateResourceName()
+	resourceName := "volterra_service_policy." + name
+	testURL, stopFunc := createTestCustomAPIServer(t, []string{ves_io_schema_ns.ObjectType})
+	defer stopFunc()
+	os.Setenv("VOLT_API_TEST", "true")
+	os.Setenv("VOLT_API_URL", testURL)
+	os.Setenv("TF_ACC", "true")
+	os.Setenv("VOLT_VESENV", "true")
+	os.Setenv("VOLT_TENANT", "ves-io")
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		PreCheck:  func() { testAccPreCheck() },
+		Steps: []resource.TestStep{
+			{
+				Config: testConfigDataSourceServicePolicy(ves_io_schema_ns.ObjectType, name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+				),
+			},
+		},
+	})
+}
+
+func testConfigDataSourceServicePolicy(objType, name string) string {
+	// convert ves.io.schema.vservice_policy.Object to volt_service_policy
+	parts := strings.Split(objType, ".")
+	tfResourceName := fmt.Sprintf("volterra_%s", parts[3])
+	return fmt.Sprintf(`
+	resource "%[1]s" "%[2]s" {
+		name = "%[2]s"
+	}
+
+	data "%[1]s" "%[2]s" {
+		name = "%[2]s"
+	}`, tfResourceName, name)
+}
