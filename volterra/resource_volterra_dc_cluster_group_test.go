f69a66d96e279ebef1779216f706e5222d18f1d7
madhukar_n <m.nayakbomman@f5.com>
2022-09-27 11:45:54 -0700

    Add tests for dns_zone and dc_cluster_grp
    ref: https://github.com/volterraedge/terraform-provider-volterra/issues/160

---
+++ volterra/resource_volterra_dc_cluster_group_test.go
@@ -0,0 +1,46 @@
+//
+// Copyright (c) 2020 Volterra, Inc. Licensed under APACHE LICENSE, VERSION 2.0
+//
+package volterra
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	ves_io_schema_dcg "github.com/volterraedge/terraform-provider-volterra/pbgo/extschema/schema/dc_cluster_group"
+)
+
+// TestDCClusterGrp object CRUD
+func TestDCClusterGrp(t *testing.T) {
+	name := generateResourceName()
+	testURL, stopFunc, _ := createTestCustomAPIServer(t, []string{
+		ves_io_schema_dcg.ObjectType,
+	})
+	defer stopFunc()
+	os.Setenv("VOLT_API_TEST", "true")
+	os.Setenv("VOLT_API_URL", testURL)
+	os.Setenv("TF_ACC", "true")
+	os.Setenv("VOLT_VESENV", "true")
+	os.Setenv("VOLT_TENANT", "ves-io")
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		PreCheck:  func() { testAccPreCheck() },
+		Steps: []resource.TestStep{
+			{
+				Config: testDCClusterGrp(name),
+			},
+		},
+	})
+}
+
+func testDCClusterGrp(name string) string {
+	return fmt.Sprintf(`
+		resource "volterra_dc_cluster_group" "dd" {
+			name = "%s"
+			namespace = "system"
+		}
+
+	`, name)
+}
